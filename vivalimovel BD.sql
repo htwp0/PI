28b0b05c893d42e5e00996f880d49c4dd5f37827
htwp0 <santosmouramariaeduarda1@gmail.com>
2025-05-08 20:51:44 -0300

    Add files via upload

---
+++ vivalimovel BD.sql
@@ -0,0 +1,25 @@
+CREATE TYPE status_tipo AS ENUM('alugado','naoalugado');
+
+create table tb_VivaImovel(
+
+id_imovel int,
+endereço varchar(200),
+numero varchar(10),
+complemento varchar(50),
+cidade varchar(10),
+estado char(2),
+cep varchar(10),
+tipo_imovel varchar(50),
+area_total float,
+area_construida float,
+quartos int,
+suites int,
+banheiros int,
+descricao int,
+iptu float,
+data_cadastro TIMESTAMP,
+valor_aluguel float,
+status_casa status_tipo
+	
+
+	);
